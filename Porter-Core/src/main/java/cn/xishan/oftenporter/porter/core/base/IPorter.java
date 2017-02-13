4b49de07f42852a2b516562287d1f56172efa722
CLovinr <759914852@qq.com>
2017-02-13 20:32:46 +0800

    完善：整理

---
+++ Porter-Core/src/main/java/cn/xishan/oftenporter/porter/core/base/IPorter.java
@@ -0,0 +1,13 @@
+package cn.xishan.oftenporter.porter.core.base;
+
+/**
+ * @author Created by https://github.com/CLovinr on 2017/2/9.
+ */
+public interface IPorter
+{
+    /**
+     * 获取接口类的绑定名。
+     * @return
+     */
+    String classTied();
+}
