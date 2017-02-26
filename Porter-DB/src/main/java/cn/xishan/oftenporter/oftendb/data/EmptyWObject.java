b58e485e86d185c9439b2e63b006bae9f41cb222
CLovinr <759914852@qq.com>
2017-02-27 00:46:21 +0800

    完善

---
+++ Porter-DB/src/main/java/cn/xishan/oftenporter/oftendb/data/EmptyWObject.java
@@ -0,0 +1,83 @@
+package cn.xishan.oftenporter.oftendb.data;
+
+import cn.xishan.oftenporter.porter.core.base.*;
+import cn.xishan.oftenporter.porter.core.pbridge.Delivery;
+import cn.xishan.oftenporter.porter.core.pbridge.PName;
+import cn.xishan.oftenporter.porter.simple.DefaultUrlDecoder;
+
+import java.util.HashMap;
+
+/**
+ * @author Created by https://github.com/CLovinr on 2017/2/27.
+ */
+public class EmptyWObject extends WObject
+{
+    private UrlDecoder.Result result;
+
+    public EmptyWObject(String contextName, String classTied, String funTied)
+    {
+        result = DefaultUrlDecoder.newResult(new HashMap<>(0), contextName, classTied, funTied);
+        cn = new Object[0];
+        cu = new Object[0];
+        cinner = new Object[0];
+        fn = new Object[0];
+        fu = new Object[0];
+        finner = new Object[0];
+        cInNames = InNames.fromStringArray(new String[0], new String[0], new String[0]);
+        fInNames = InNames.fromStringArray(new String[0], new String[0], new String[0]);
+    }
+
+    @Override
+    public WRequest getRequest()
+    {
+        throw new RuntimeException("not allowed");
+    }
+
+    @Override
+    public WResponse getResponse()
+    {
+        throw new RuntimeException("not allowed");
+    }
+
+    @Override
+    public <T> T finObject(int index)
+    {
+        throw new RuntimeException("not allowed");
+    }
+
+    @Override
+    public <T> T cinObject(int index)
+    {
+        throw new RuntimeException("not allowed");
+    }
+
+    @Override
+    public <T> T savedObject(String key)
+    {
+        throw new RuntimeException("not allowed");
+    }
+
+    @Override
+    public <T> T gsavedObject(String key)
+    {
+        throw new RuntimeException("not allowed");
+    }
+
+    @Override
+    public Delivery delivery()
+    {
+        throw new RuntimeException("not allowed");
+    }
+
+    @Override
+    public UrlDecoder.Result url()
+    {
+        return url();
+    }
+
+    @Override
+    public PName getPName()
+    {
+        throw new RuntimeException("not allowed");
+    }
+}
