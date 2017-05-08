adb276a20801f5cfd570979f0dde1896331749eb
CLovinr <759914852@qq.com>
2017-05-08 23:52:01 +0800

    完善:jbatis

--- Porter-DB/src/main/java/cn/xishan/oftenporter/oftendb/jbatis/JsInterface.java
+++ Porter-DB/src/main/java/cn/xishan/oftenporter/oftendb/jbatis/JsInterface.java
@@ -8,6 +8,8 @@ import cn.xishan.oftenporter.oftendb.db.mysql.SqlUtil;
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
 import jdk.nashorn.internal.objects.NativeArray;
 
+import java.util.Map;
+
 /**
  * Created by chenyg on 2017-04-29.
  */
@@ -36,27 +38,60 @@ public class JsInterface
 
     public JSqlArgs sqlArgs(String sql, Object argsObj)
     {
-        Object[] args;
-        if (argsObj == null)
+        Object[] args = toArray(argsObj);
+        return new JSqlArgs(sql, args);
+    }
+
+    public static Object[] toArray(Object object)
+    {
+        if (object == null)
         {
-            args = null;
-        } else if (argsObj instanceof Object[])
+            return null;
+        } else if (object instanceof ScriptObjectMirror)
         {
-            args = (Object[]) argsObj;
-        } else if (argsObj instanceof NativeArray)
+            ScriptObjectMirror scriptObjectMirror = (ScriptObjectMirror) object;
+            if (!scriptObjectMirror.isArray())
+            {
+                throw new IllegalArgumentException("ScriptObjectMirror is no array");
+            }
+
+            if (scriptObjectMirror.isEmpty())
+            {
+                return new Object[0];
+            }
+
+            Object[] array = new Object[scriptObjectMirror.size()];
+
+            int i = 0;
+
+            for (Map.Entry<String, Object> entry : scriptObjectMirror.entrySet())
+            {
+                Object result = entry.getValue();
+                if (result == null)
+                {
+                    array[i] = result;
+                } else if (result instanceof ScriptObjectMirror && scriptObjectMirror.isArray())
+                {
+                    array[i] = toArray(result);
+                } else if (result instanceof NativeArray)
+                {
+                    array[i] = ((NativeArray) result).asObjectArray();
+                } else
+                {
+                    array[i] = result;
+                }
+                i++;
+            }
+
+            return array;
+        } else if (object instanceof NativeArray)
         {
-            NativeArray nativeArray = (NativeArray) argsObj;
-            args = nativeArray.asObjectArray();
+            Object[] array = ((NativeArray) object).asObjectArray();
+            return array;
         } else
         {
-            ScriptObjectMirror scriptObjectMirror = (ScriptObjectMirror) argsObj;
-            args = new Object[scriptObjectMirror.size()];
-            for (int i = 0; i < args.length; i++)
-            {
-                args[i] = scriptObjectMirror.get(i + "");
-            }
+            throw new IllegalArgumentException("it is not an array:" + object);
         }
-        return new JSqlArgs(sql, args);
     }
 
     public String filterLike(String content)
