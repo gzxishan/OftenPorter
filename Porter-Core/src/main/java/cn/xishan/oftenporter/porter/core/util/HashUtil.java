6ed3356c5f4e10084d2a4771aa3c4e6d33120ffb
zggzcyg@qq.com <zggzcyg@qq.com>
2017-11-06 23:20:42 +0800

    完善

--- Porter-Core/src/main/java/cn/xishan/oftenporter/porter/core/util/HashUtil.java
+++ Porter-Core/src/main/java/cn/xishan/oftenporter/porter/core/util/HashUtil.java
@@ -1,5 +1,6 @@
 package cn.xishan.oftenporter.porter.core.util;
 
+import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -9,6 +10,11 @@ public class HashUtil
     public static String md5(byte[] bytes, int... offsetAndLength)
     {
         return hashHex(bytes, "MD5", offsetAndLength);
+    }
+
+    public static String md5(InputStream in, int bufSize)
+    {
+        return hashHex("MD5", in, bufSize);
     }
 
 
@@ -21,9 +27,20 @@ public class HashUtil
         return md5Str.substring(8, 24);
     }
 
+    public static String md5_16(InputStream in, int bufSize)
+    {
+        String md5Str = md5(in, bufSize);
+        return md5Str.substring(8, 24);
+    }
+
     public static String sha1(byte[] bytes, int... offsetAndLength)
     {
         return hashHex(bytes, "sha-1", offsetAndLength);
+    }
+
+    public static String sha1(InputStream in, int bufSize)
+    {
+        return hashHex("sha-1", in, bufSize);
     }
 
     public static String sha256(byte[] bytes, int... offsetAndLength)
@@ -31,9 +48,19 @@ public class HashUtil
         return hashHex(bytes, "sha-256", offsetAndLength);
     }
 
+    public static String sha256(InputStream in, int bufSize)
+    {
+        return hashHex("sha-256", in, bufSize);
+    }
+
     public static String sha384(byte[] bytes, int... offsetAndLength)
     {
         return hashHex(bytes, "sha-384", offsetAndLength);
+    }
+
+    public static String sha384(InputStream in, int bufSize)
+    {
+        return hashHex("sha-384", in, bufSize);
     }
 
     public static String sha512(byte[] bytes, int... offsetAndLength)
@@ -41,22 +68,39 @@ public class HashUtil
         return hashHex(bytes, "sha-512", offsetAndLength);
     }
 
+    public static String sha512(InputStream in, int bufSize)
+    {
+        return hashHex("sha-512", in, bufSize);
+    }
+
 
     /**
      * @param bs              待hash的数据
      * @param sname           hash算法的名称
      * @param offsetAndLength 大小为0或2
-     * @return 返回16进制hash值
+     * @return 返回16进制hash值(小写)
      */
     public static String hashHex(byte[] bs, String sname, int... offsetAndLength)
     {
         byte[] bytes = hash(bs, sname, offsetAndLength);
-        return BytesTool.toHex(bytes, 0, bytes.length,true);
+        return BytesTool.toHex(bytes, 0, bytes.length, true);
+    }
+
+
+    /**
+     * @param sname hash算法的名称
+     * @param in
+     * @return 返回16进制hash值(小写)
+     */
+    public static String hashHex(String sname, InputStream in, int bufSize)
+    {
+        byte[] bytes = hash(sname, in, bufSize);
+        return BytesTool.toHex(bytes, 0, bytes.length, true);
     }
 
     /**
      * @param bs              待hash的数据
-     * @param sname           hash算法的名称
+     * @param sname           hash算法的名称,如：MD5,sha-1,sha-256,sha-384,sha-512
      * @param offsetAndLength 大小为0或2
      * @return 返回hash值
      */
@@ -76,6 +120,39 @@ public class HashUtil
         } catch (NoSuchAlgorithmException e)
         {
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @param bs              待hash的数据
+     * @param sname           hash算法的名称
+     * @param offsetAndLength 大小为0或2
+     * @return 返回hash值
+     */
+    /**
+     * @param sname   hash算法的名称,如：MD5,sha-1,sha-256,sha-384,sha-512
+     * @param in      待hash的数据流
+     * @param bufSize
+     * @return
+     */
+    public static byte[] hash(String sname, InputStream in, int bufSize)
+    {
+        try
+        {
+            MessageDigest digest = MessageDigest.getInstance(sname);
+            byte[] buf = new byte[bufSize];
+            int n;
+            while ((n = in.read(buf)) != -1)
+            {
+                digest.update(buf, 0, n);
+            }
+            return digest.digest();
+        } catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        } finally
+        {
+            WPTool.close(in);
         }
     }
 
