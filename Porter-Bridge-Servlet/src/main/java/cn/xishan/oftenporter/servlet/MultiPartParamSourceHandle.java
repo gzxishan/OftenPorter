ac1244276d1730985a037ede8848458e79c0018c
zggzcyg@qq.com <zggzcyg@qq.com>
2017-11-08 20:50:25 +0800

    完善

--- Porter-Bridge-Servlet/src/main/java/cn/xishan/oftenporter/servlet/MultiPartParamSourceHandle.java
+++ Porter-Bridge-Servlet/src/main/java/cn/xishan/oftenporter/servlet/MultiPartParamSourceHandle.java
@@ -17,6 +17,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,48 +25,59 @@ import java.util.Map;
 /**
  * @author Created by https://github.com/CLovinr on 2017/4/15.
  */
-class MultiPartParamSourceHandle extends PutParamSourceHandle {
+class MultiPartParamSourceHandle extends PutParamSourceHandle
+{
     private MultiPartOption multiPartOption;
     private DiskFileItemFactory factory;
     private boolean dealNormalPut;
 
-    public MultiPartParamSourceHandle(MultiPartOption multiPartOption, boolean dealNormalPut) {
+    public MultiPartParamSourceHandle(MultiPartOption multiPartOption, boolean dealNormalPut)
+    {
         this.multiPartOption = multiPartOption;
         this.dealNormalPut = dealNormalPut;
         factory = new DiskFileItemFactory();
-        if (multiPartOption.tempDir != null) {
+        if (multiPartOption.tempDir != null)
+        {
             factory.setRepository(new File(multiPartOption.tempDir));
         }
         factory.setSizeThreshold(multiPartOption.cacheSize);
     }
 
     @Override
-    public ParamSource get(WObject wObject, Class<?> porterClass, Method porterFun) throws Exception {
+    public ParamSource get(WObject wObject, Class<?> porterClass, Method porterFun) throws Exception
+    {
         ParamSource paramSource = _get(wObject, porterClass, porterFun);
-        if (paramSource == null && dealNormalPut && wObject.getRequest().getMethod() == PortMethod.PUT) {
+        if (paramSource == null && dealNormalPut && wObject.getRequest().getMethod() == PortMethod.PUT)
+        {
             paramSource = super.get(wObject, porterClass, porterFun);
         }
         return paramSource;
     }
 
-    ParamSource _get(WObject wObject, Class<?> porterClass, Method porterFun) throws Exception {
+    ParamSource _get(WObject wObject, Class<?> porterClass, Method porterFun) throws Exception
+    {
 
 
         Object originalRequest = wObject.getRequest().getOriginalRequest();
-        if (originalRequest == null || !(originalRequest instanceof HttpServletRequest)) {
+        if (originalRequest == null || !(originalRequest instanceof HttpServletRequest))
+        {
             return null;
         }
         HttpServletRequest request = (HttpServletRequest) originalRequest;
-        if (ContentType.APP_JSON.name().equals(request.getContentType())) {
-            if (multiPartOption.decodeJsonParams) {
+        if (ContentType.APP_JSON.name().equals(request.getContentType()))
+        {
+            if (multiPartOption.decodeJsonParams)
+            {
                 JSONObject jsonObject = JSON.parseObject(
                         FileTool.getString(request.getInputStream(), 1024, request.getCharacterEncoding()));
                 ParamSource paramSource = new DefaultParamSource(jsonObject, wObject.getRequest());
                 return paramSource;
-            } else {
+            } else
+            {
                 return null;
             }
-        } else if (!ServletFileUpload.isMultipartContent(request)) {
+        } else if (!ServletFileUpload.isMultipartContent(request))
+        {
             return null;
         }
 
@@ -73,34 +85,65 @@ class MultiPartParamSourceHandle extends PutParamSourceHandle {
         upload.setFileSizeMax(multiPartOption.maxBytesEachFile);
         upload.setSizeMax(multiPartOption.totalBytes);
         Map<String, Object> map = new HashMap<>();
+        Map<String, List<Object[]>> filesMap = new HashMap<>(2);
+
         List<FileItem> list = upload.parseRequest(request);
-        for (FileItem fileItem : list) {
+        for (FileItem fileItem : list)
+        {
             String name = fileItem.getFieldName();
             Object value;
-            if (fileItem.isFormField()) {
+            if (fileItem.isFormField())
+            {
                 String encoding = request.getCharacterEncoding();
-                if(WPTool.isEmpty(encoding)){
-                    encoding="utf-8";
+                if (WPTool.isEmpty(encoding))
+                {
+                    encoding = "utf-8";
                 }
                 value = fileItem.getString(encoding);
-            } else {
+                map.put(name, value);
+            } else
+            {
                 File tempFile;
-                if (fileItem.isInMemory()) {
+                if (fileItem.isInMemory())
+                {
                     tempFile = File.createTempFile(KeyUtil.randomUUID(), ".temp", new File(multiPartOption.tempDir));
                     FileTool.write2File(fileItem.getInputStream(), tempFile, true);
-                } else {
+                } else
+                {
                     DiskFileItem diskFileItem = (DiskFileItem) fileItem;
                     tempFile = diskFileItem.getStoreLocation();
                 }
 
                 String origin = fileItem.getName();
-                if (WPTool.isEmpty(origin)) {
-                    origin = name;
+                if (WPTool.isEmpty(origin))
+                {
+                    origin = KeyUtil.randomUUID();
                 }
-                value = new FilePart(origin, tempFile);
 
+                List<Object[]> files = filesMap.get(name);
+                if (files == null)
+                {
+                    files = new ArrayList<>(2);
+                    filesMap.put(name, files);
+                }
+                files.add(new Object[]{origin, tempFile});
             }
-            map.put(name, value);
+        }
+
+        for (Map.Entry<String, List<Object[]>> entry : filesMap.entrySet())
+        {
+            List<Object[]> filesList = entry.getValue();
+            String[] origins = new String[filesList.size()];
+            File[] files = new File[filesList.size()];
+
+            for (int i = 0; i < origins.length; i++)
+            {
+                Object[] objs = filesList.get(i);
+                origins[i] = (String) objs[0];
+                files[i] = (File) objs[1];
+            }
+
+            map.put(entry.getKey(), new FilePart(origins, files));
         }
 
         ParamSource paramSource = new DefaultParamSource(map, wObject.getRequest());
