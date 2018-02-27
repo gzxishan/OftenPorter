607c4c734947a5ea5989e62010b0377b8edc1378
759914852@qq.com <49101919201o>
2018-02-27 14:43:50 +0800

    完善

---
+++ Porter-Core/src/test/java/cn/xishan/oftenporter/porter/core/annotation/AnnotationTest.java
@@ -0,0 +1,29 @@
+package cn.xishan.oftenporter.porter.core.annotation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.annotation.Annotation;
+
+
+
+/**
+ * Created by chenyg on 2018-02-27.
+ */
+@PortComment
+@Mixin
+@PortIn
+public class AnnotationTest
+{
+    /**
+     * 用于测试注解顺序。
+     */
+    @Test
+    public void testAnnotationOrder()
+    {
+        Annotation[] annotations = getClass().getAnnotations();
+        Assert.assertEquals(PortComment.class, annotations[0].annotationType());
+        Assert.assertEquals(Mixin.class, annotations[1].annotationType());
+        Assert.assertEquals(PortIn.class, annotations[2].annotationType());
+    }
+}
