7f63fef36d18140cc9f673e63cdb1e440460e044
CLovinr <759914852@qq.com>
2018-05-09 15:56:38 +0800

    完善：Mybatis

--- Porter-DB/src/main/java/cn/xishan/oftenporter/oftendb/mybatis/MybatisConfig.java
+++ Porter-DB/src/main/java/cn/xishan/oftenporter/oftendb/mybatis/MybatisConfig.java
@@ -1,5 +1,7 @@
 package cn.xishan.oftenporter.oftendb.mybatis;
 
+import cn.xishan.oftenporter.porter.core.annotation.AutoSet;
+
 /**
  * @author Created by https://github.com/CLovinr on 2017/12/6.
  */
@@ -7,6 +9,10 @@ class MybatisConfig
 {
     MyBatisOption myBatisOption;
     MSqlSessionFactoryBuilder mSqlSessionFactoryBuilder;
+
+    //确保至少有一个Dao，从而会进行初始化
+    @AutoSet
+    MyBatisDao myBatisDao;
 
     public MybatisConfig(MyBatisOption myBatisOption,
             MSqlSessionFactoryBuilder mSqlSessionFactoryBuilder)
