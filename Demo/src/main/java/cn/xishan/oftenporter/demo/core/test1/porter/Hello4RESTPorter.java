445ae274bca7c22d182b3cb0cb97580755b625e8
CLovinr <zggzcyg@qq.xom>
2018-06-05 01:05:10 +0800

    完善:maven

--- Demo/src/main/java/cn/xishan/oftenporter/demo/core/test1/porter/Hello4RESTPorter.java
+++ Demo/src/main/java/cn/xishan/oftenporter/demo/core/test1/porter/Hello4RESTPorter.java
@@ -10,28 +10,26 @@ import cn.xishan.oftenporter.porter.core.base.WObject;
  * 1.设置PortIn.tiedType=true，使得接口类中，使得各个类型的接口函数都有效。
  * 	不过非REST函数会有先于REST函数。
  * </pre>
- * 
- * @author https://github.com/CLovinr <br>
- *         2016年9月16日 下午4:57:02
  *
+ * @author https://github.com/CLovinr 2016年9月16日 下午4:57:02
  */
 @PortIn(tiedType = TiedType.REST)
 public class Hello4RESTPorter
 {
 
-    @PortIn(nece = { "name" }, unece = { "msg" }, method = PortMethod.POST,
-	    tiedType = TiedType.REST)
+    @PortIn(nece = {"name"}, unece = {"msg"}, method = PortMethod.POST,
+            tiedType = TiedType.REST)
     public Object add(WObject wObject)
     {
-	String name = (String) wObject.fn[0];
-	Object msg = wObject.fu[0];
+        String name = (String) wObject.fn[0];
+        Object msg = wObject.fu[0];
 
-	return name + ":" + msg + ":" + wObject.restValue;
+        return name + ":" + msg + ":" + wObject.restValue;
     }
 
     @PortIn(value = "add", nece = "content", method = PortMethod.POST)
     public Object add2(WObject wObject)
     {
-	return "content:" + wObject.fn[0];
+        return "content:" + wObject.fn[0];
     }
 }
