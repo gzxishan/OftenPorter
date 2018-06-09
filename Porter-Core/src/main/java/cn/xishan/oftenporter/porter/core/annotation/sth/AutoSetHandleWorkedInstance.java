8194488e8196efc370f8cf829770ba77e9ce74c8
CLovinr <zggzcyg@qq.xom>
2018-06-09 14:21:38 +0800

    修复bug：object.getClass().getPackage()返回null的问题

--- Porter-Core/src/main/java/cn/xishan/oftenporter/porter/core/annotation/sth/AutoSetHandleWorkedInstance.java
+++ Porter-Core/src/main/java/cn/xishan/oftenporter/porter/core/annotation/sth/AutoSetHandleWorkedInstance.java
@@ -8,35 +8,45 @@ import java.util.Map;
 /**
  * Created by https://github.com/CLovinr on 2017/9/28.
  */
-class AutoSetHandleWorkedInstance {
+class AutoSetHandleWorkedInstance
+{
 
     private Map<Integer, List<Object>> worked = new HashMap<>();
 
-    public synchronized void clear() {
+    public synchronized void clear()
+    {
         worked.clear();
     }
 
-    public synchronized boolean workInstance(Object object) {
+    public synchronized boolean workInstance(Object object)
+    {
         boolean isWorked = false;
-        if (object != null) {
-
-            if(object.getClass().getPackage().getName().startsWith("java.")){
+        if (object != null)
+        {
+            Package p = object.getClass().getPackage();
+            if (p == null || p.getName().startsWith("java."))
+            {
                 return true;
             }
 
             List<Object> list = worked.get(object.hashCode());
-            if (list == null) {
+            if (list == null)
+            {
                 list = new ArrayList<>();
                 worked.put(object.hashCode(), list);
                 list.add(object);
-            } else {
-                for (int i = 0; i < list.size(); i++) {
-                    if (list.get(i) == object) {
+            } else
+            {
+                for (int i = 0; i < list.size(); i++)
+                {
+                    if (list.get(i) == object)
+                    {
                         isWorked = true;
                         break;
                     }
                 }
-                if (!isWorked) {
+                if (!isWorked)
+                {
                     list.add(object);
                 }
             }
