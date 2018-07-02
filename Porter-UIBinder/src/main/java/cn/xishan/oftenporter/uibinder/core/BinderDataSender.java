9f89e592fc6578a3dcf5cf0a897be43be463639c
CLovinr <759914852@qq.com>
2018-07-02 18:23:40 +0800

    先删除UI部分

--- Porter-UIBinder/src/main/java/cn/xishan/oftenporter/uibinder/core/BinderDataSender.java
+++
@@ -1,9 +0,0 @@
-package cn.xishan.oftenporter.uibinder.core;
-
-/**
- * Created by 宇宙之灵 on 2016/4/28.
- */
-public interface BinderDataSender
-{
-    void sendBinderData(String pathPrefix, BinderData binderData);
-}
--- Porter-UIBinder/src/main/java/cn/xishan/oftenporter/uibinder/core/BinderDefault.java
+++
@@ -1,9 +0,0 @@
-package cn.xishan.oftenporter.uibinder.core;
-
-/**
- * @author Created by https://github.com/CLovinr on 2016/10/2.
- */
-public final class BinderDefault
-{
-    public static final String CALLBACK = "callback";
-}
