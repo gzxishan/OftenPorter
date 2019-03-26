8bfe095aa88ca34faac41af1d3772c3d8931a09f
CLovinr <zggzcyg@qq.xom>
2019-03-26 17:02:58 +0800

    完善

--- Porter-Bridge-Servlet/src/main/java/cn/xishan/oftenporter/servlet/render/htmlx/Htmlx.java
+++ Porter-Bridge-Servlet/src/main/java/cn/xishan/oftenporter/servlet/render/htmlx/Htmlx.java
@@ -67,17 +67,11 @@ public @interface Htmlx
      *
      * @return
      */
-    String defaultHtml() default "<!DOCTYPE html>\n" +
-            "<html>\n" +
-            "\t<head>\n" +
-            "\t\t<meta charset=\"UTF-8\">\n" +
-            "\t\t<title></title>\n" +
-            "\t\t<meta name=\"viewport\" content=\"width=device-width,minimum-scale=1.0,maximum-scale=1" +
-            ".0," +
-            "user-scalable=no\" />\n" +
-            "\t</head>\n" +
-            "\t<body>\n" +
-            "\t\t\n" +
-            "\t</body>\n" +
-            "</html>";
+    String defaultHtml() default "<!DOCTYPE html><html><head>" +
+            "<meta charset=\"UTF-8\">" +
+            "<title></title>" +
+            "<meta name=\"viewport\" " +
+            "content=\"width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no\" />" +
+            "</head>" +
+            "<body></body></html>";
 }
