c6cd963ba76a891459fd4d8a94b594304dc19def
CLovinr <759914852@qq.com>
2019-05-31 17:45:47 +0800

    完善缓存工具

--- Porter-Bridge-Servlet/src/main/java/cn/xishan/oftenporter/servlet/HttpCacheUtil.java
+++ Porter-Bridge-Servlet/src/main/java/cn/xishan/oftenporter/servlet/HttpCacheUtil.java
@@ -1,5 +1,7 @@
 package cn.xishan.oftenporter.servlet;
 
+import cn.xishan.oftenporter.porter.core.util.OftenTool;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,6 +11,237 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class HttpCacheUtil
 {
+    public interface Callback
+    {
+        void call(HttpServletRequest request, HttpServletResponse response);
+    }
+
+    /**
+     * 最终缓存是否有效的判断：
+     * <pre>
+     *          boolean isChanged = false;
+     *             if (isChangeByDtime != null)
+     *             {
+     *                 isChanged = isChangeByDtime;
+     *             }
+     *
+     *             if (!isChanged)
+     *             {
+     *                 if (setChange != null)
+     *                 {
+     *                     isChanged = setChange || checkChange;
+     *                 } else
+     *                 {
+     *                     isChanged = checkChange;
+     *                 }
+     *             }
+     * </pre>
+     */
+    public interface ICtrl
+    {
+
+        /**
+         * 改变时的回调
+         *
+         * @param callback
+         * @return
+         */
+        ICtrl changed(Callback callback);
+
+        /**
+         * 设置缓存信息
+         *
+         * @param forceSeconds
+         * @param etag
+         * @return
+         */
+        ICtrl cacheInfo(int forceSeconds, String etag);
+
+        /**
+         * 未改变时、返回304状态。
+         *
+         * @return
+         */
+        ICtrl unchanged304();
+
+        /**
+         * @param forceSeconds 单位秒
+         * @param lastModified 单位毫秒
+         * @return
+         */
+        ICtrl cacheInfo(int forceSeconds, long lastModified);
+
+        /**
+         * 未改变时的回调
+         *
+         * @param callback
+         * @return
+         */
+        ICtrl unchanged(Callback callback);
+
+        /**
+         * 手动设置资源是否改变。
+         *
+         * @param isChange
+         * @return
+         */
+        ICtrl setIsChange(boolean isChange);
+
+        /**
+         * 如果上次访问的时间到此时大于dseconds、则判断缓存失效，另见{@linkplain #setIsChange(boolean)}。
+         *
+         * @param dseconds
+         * @return
+         */
+        ICtrl setIsChangeByDtime(int dseconds);
+
+        /**
+         * 完成设置
+         */
+        void done();
+    }
+
+    private static class ICtrlImpl implements ICtrl
+    {
+        private HttpServletRequest request;
+        private HttpServletResponse response;
+        private Callback changedCallback;
+        private Callback unchangedCallback;
+        private Integer forceSeconds;
+        private String etag;
+        private Long lastModified;
+        private Boolean response304;
+        private Boolean isChangeByDtime;
+        private Boolean setChange;
+        private boolean checkChange;
+
+        public ICtrlImpl(HttpServletRequest request, HttpServletResponse response, boolean isChanged)
+        {
+            this.request = request;
+            this.response = response;
+            this.checkChange = isChanged;
+        }
+
+        @Override
+        public ICtrl changed(Callback callback)
+        {
+            this.changedCallback = callback;
+            return this;
+        }
+
+        @Override
+        public ICtrl cacheInfo(int forceSeconds, String etag)
+        {
+            this.forceSeconds = forceSeconds;
+            this.etag = etag;
+            return this;
+        }
+
+        @Override
+        public ICtrl unchanged304()
+        {
+            response304 = true;
+            return this;
+        }
+
+        @Override
+        public ICtrl cacheInfo(int forceSeconds, long lastModified)
+        {
+            this.forceSeconds = forceSeconds;
+            this.lastModified = lastModified;
+            return this;
+        }
+
+        @Override
+        public ICtrl unchanged(Callback callback)
+        {
+            this.unchangedCallback = changedCallback;
+            return this;
+        }
+
+        @Override
+        public ICtrl setIsChange(boolean isChange)
+        {
+            this.setChange = isChange;
+            return this;
+        }
+
+        @Override
+        public ICtrl setIsChangeByDtime(int dseconds)
+        {
+            long since = request.getDateHeader("If-Modified-Since") / 1000;
+            this.isChangeByDtime = (System.currentTimeMillis() / 1000 - since) > dseconds;
+            return this;
+        }
+
+        @Override
+        public void done()
+        {
+            boolean isChanged = false;
+            if (isChangeByDtime != null)
+            {
+                isChanged = isChangeByDtime;
+            }
+
+            if (!isChanged)
+            {
+                if (setChange != null)
+                {
+                    isChanged = setChange || checkChange;
+                } else
+                {
+                    isChanged = checkChange;
+                }
+            }
+
+            if (isChanged)
+            {
+                if (changedCallback != null)
+                {
+                    changedCallback.call(request, response);
+                }
+            } else
+            {
+                if (forceSeconds != null)
+                {
+                    if (OftenTool.notEmpty(etag))
+                    {
+                        setCacheWithEtag(forceSeconds, etag, response);
+                    }
+
+                    if (OftenTool.notEmpty(lastModified))
+                    {
+                        setCacheWithModified(forceSeconds, lastModified, response);
+                    }
+                }
+
+                if (unchangedCallback != null)
+                {
+                    unchangedCallback.call(request, response);
+                }
+                if (response304 != null)
+                {
+                    notModified(response);
+                }
+            }
+        }
+    }
+
+
+    public static ICtrl checkWithEtag(String etag, HttpServletRequest request,
+            HttpServletResponse response)
+    {
+        ICtrl ctrl = new ICtrlImpl(request, response, isCacheIneffectiveWithEtag(etag, request, response));
+        return ctrl;
+    }
+
+    public static ICtrl checkWithModified(long lastModified, HttpServletRequest request,
+            HttpServletResponse response)
+    {
+        ICtrl ctrl = new ICtrlImpl(request, response, isCacheIneffectiveWithModified(lastModified, request, response));
+        return ctrl;
+    }
+
     /**
      * 检测资源是否过期，若过期了则重新设置缓存时间.
      *
@@ -125,7 +358,8 @@ public class HttpCacheUtil
             HttpServletResponse response)
     {
         long since = request.getDateHeader("If-Modified-Since") / 1000;
-        if(lastModified>0){
+        if (lastModified > 0)
+        {
             lastModified /= 1000;
         }
         if (lastModified == since)
